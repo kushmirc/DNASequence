0770e7d8172b7b7a694f16e6f215c631d42f1286
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-26 15:51:56 -0600

    Call .indexOf() to find start and stop codons

--- src/DNASequence/DNA.java
+++ src/DNASequence/DNA.java
@@ -10,6 +10,11 @@ public class DNA {
         String dna = dna3;
         System.out.println(dna.length());
 
+        //find index of start codon
+        System.out.println(dna.indexOf("ATG"));
+        //find index of stop codon
+        System.out.println(dna.indexOf("TGA"));
+
 
     }
 
