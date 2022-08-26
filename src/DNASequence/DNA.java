ac973465aaca935b6dbc5d73b28f2f21bfa10717
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-26 14:44:02 -0600

    Add 3 sample DNA strings

--- src/DNASequence/DNA.java
+++ src/DNASequence/DNA.java
@@ -3,7 +3,9 @@ package DNASequence;
 public class DNA {
 
     public static void main(String[] args) {
-
+        String dna1 = "ATGCGATACGCTTGA";
+        String dna2 = "ATGCGATACGTGA";
+        String dna3 = "ATTAATATGTACTGA";
     }
 
 }
