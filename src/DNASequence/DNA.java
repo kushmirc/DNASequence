72e7148c5cd64442609fb46da8611cc1f8205922
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-26 15:50:10 -0600

    Add general dna variable and call .length() on it

--- src/DNASequence/DNA.java
+++ src/DNASequence/DNA.java
@@ -6,6 +6,11 @@ public class DNA {
         String dna1 = "ATGCGATACGCTTGA";
         String dna2 = "ATGCGATACGTGA";
         String dna3 = "ATTAATATGTACTGA";
+
+        String dna = dna3;
+        System.out.println(dna.length());
+
+
     }
 
 }
